7326b052c8c29ee06044e849fbbf1ee5a17c690a
Sacha Markarian <markarian696@icloud.com>
2025-01-30 13:09:40 +0100

    commit maj

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      './src/**/*.{html,js}',  // Chemins vers tes fichiers HTML ou JS
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
